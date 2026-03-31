bf3f87594190c9137f2b3d285f4d59edd007731a
chamikarab <chamikara38@gmail.com>
2026-04-01 02:21:14 +0530

    refactor: update cart service routes and OpenAPI specifications to use a unified path structure

--- cart-service/src/routes/cartRoutes.js
+++ cart-service/src/routes/cartRoutes.js
@@ -5,17 +5,17 @@ const asyncHandler = require("../utils/asyncHandler");
 
 const router = express.Router();
 
-router.get("/carts", asyncHandler(cartController.listCarts));
-router.get("/carts/:userId", asyncHandler(cartController.getCart));
-router.post("/carts/:userId/items", asyncHandler(cartController.addCartItem));
+router.get("/", asyncHandler(cartController.listCarts));
+router.get("/:userId", asyncHandler(cartController.getCart));
+router.post("/:userId/items", asyncHandler(cartController.addCartItem));
 router.patch(
-  "/carts/:userId/items/:itemId",
+  "/:userId/items/:itemId",
   asyncHandler(cartController.updateCartItem)
 );
 router.delete(
-  "/carts/:userId/items/:itemId",
+  "/:userId/items/:itemId",
   asyncHandler(cartController.removeCartItem)
 );
-router.delete("/carts/:userId/items", asyncHandler(cartController.clearUserCart));
+router.delete("/:userId/items", asyncHandler(cartController.clearUserCart));
 
 module.exports = router;
